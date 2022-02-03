b204947dfdac62b19b1bc01f12a89068a16ee75c
oliver7598 <oliver.a@live.co.uk>
2022-02-03 13:57:08 +0000

    Ammended error in script

--- templates/workspace_services/guacamole/user_resources/guacamole-azure-ubuntuvm-cloud-init/terraform/rdp_config.sh
+++ templates/workspace_services/guacamole/user_resources/guacamole-azure-ubuntuvm-cloud-init/terraform/rdp_config.sh
@@ -2,8 +2,8 @@
 NEXUS_URL="https://nexus-${tre_id}.azurewebsites.net"
 sudo rm -r /var/lib/apt/lists/*
 sudo rm /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-proxy-repo bionic main restricted universe multiverse" >>
-echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-proxy-repo bionic-updates main restricted universe multiverse" >>
+echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-proxy-repo bionic main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-proxy-repo bionic-updates main restricted universe multiverse" >> /etc/apt/sources.list
 echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-security-proxy-repo bionic main restricted universe multiverse" >> /etc/apt/sources.list
 echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-security-proxy-repo bionic-updates main restricted universe multiverse" >> /etc/apt/sources.list
 echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-packages-proxy-repo bionic main restricted universe multiverse" >> /etc/apt/sources.list
