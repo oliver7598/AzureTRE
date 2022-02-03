e4695450a07c7acf71c26213769eccc8c122f8fe
oliver7598 <oliver.a@live.co.uk>
2022-02-03 13:10:55 +0000

    Updated sources.list urls

--- templates/workspace_services/guacamole/user_resources/guacamole-azure-ubuntuvm-cloud-init/terraform/rdp_config.sh
+++ templates/workspace_services/guacamole/user_resources/guacamole-azure-ubuntuvm-cloud-init/terraform/rdp_config.sh
@@ -2,18 +2,18 @@
 NEXUS_URL="https://nexus-${tre_id}.azurewebsites.net"
 sudo rm -r /var/lib/apt/lists/*
 sudo rm /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/service/rest/v1/repositories/apt/proxy/ubuntu-proxy-repo bionic main restricted" >>
-echo "deb [trusted=yes] $NEXUS_URL/service/rest/v1/repositories/apt/proxy/ubuntu-proxy-repo bionic universe" >> /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/service/rest/v1/repositories/apt/proxy/ubuntu-proxy-repo bionic multiverse" >> /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/service/rest/v1/repositories/apt/proxy/ubuntu-security-proxy-repo bionic main restricted" >> /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/service/rest/v1/repositories/apt/proxy/ubuntu-security-proxy-repo bionic universe" >> /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/service/rest/v1/repositories/apt/proxy/ubuntu-security-proxy-repo bionic multiverse" >> /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/service/rest/v1/repositories/apt/proxy/ubuntu-packages-proxy-repo bionic main restricted" >> /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/service/rest/v1/repositories/apt/proxy/ubuntu-packages-proxy-repo bionic universe" >> /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/service/rest/v1/repositories/apt/proxy/ubuntu-packages-proxy-repo bionic multiverse" >> /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/service/rest/v1/repositories/apt/proxy/pypi-proxy-repo bionic main restricted" >> /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/service/rest/v1/repositories/apt/proxy/pypi-proxy-repo bionic universe" >> /etc/apt/sources.list
-echo "deb [trusted=yes] $NEXUS_URL/service/rest/v1/repositories/apt/proxy/pypi-proxy-repo bionic multiverse" >> /etc/apt/sources.list
+echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-proxy-repo bionic main restricted" >>
+echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-proxy-repo bionic universe" >> /etc/apt/sources.list
+echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-proxy-repo bionic multiverse" >> /etc/apt/sources.list
+echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-security-proxy-repo bionic main restricted" >> /etc/apt/sources.list
+echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-security-proxy-repo bionic universe" >> /etc/apt/sources.list
+echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-security-proxy-repo bionic multiverse" >> /etc/apt/sources.list
+echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-packages-proxy-repo bionic main restricted" >> /etc/apt/sources.list
+echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-packages-proxy-repo bionic universe" >> /etc/apt/sources.list
+echo "deb [trusted=yes] $NEXUS_URL/repository/ubuntu-packages-proxy-repo bionic multiverse" >> /etc/apt/sources.list
+echo "deb [trusted=yes] $NEXUS_URL/repository/pypi-proxy-repo bionic main restricted" >> /etc/apt/sources.list
+echo "deb [trusted=yes] $NEXUS_URL/repository/pypi-proxy-repo bionic universe" >> /etc/apt/sources.list
+echo "deb [trusted=yes] $NEXUS_URL/repository/pypi-proxy-repo bionic multiverse" >> /etc/apt/sources.list
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install ubuntu-gnome-desktop -yq
 sudo apt-get install xrdp -y
