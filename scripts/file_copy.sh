403ad055c6daca08fa4cf344849b40603c6a2058
oliver7598 <oliver.a@live.co.uk>
2022-03-25 12:24:36 +0000

    Formatting fix

--- scripts/file_copy.sh
+++ scripts/file_copy.sh
@@ -1,5 +1,4 @@
 #!bin/bash
-
 Help()
 {
    # Display Help
@@ -18,66 +17,66 @@ Help()
 }
 
 while [ "$1" != "" ]; do
+  case $1 in
+  --airlock-sa)
+    shift
+    airlock_sa=$1
+    ;;
+  --workspace-sa)
+    shift
+    workspace_sa=$1
+    ;;
+  --airlock-key)
+    shift
+    airlock_key=$1
+    ;;
+  --workspace-key)
+    shift
+    workspace_key=$1
+    ;;
+  --operation-type)
+    shift
     case $1 in
-    --airlock-sa)
-        shift
-        airlock_sa=$1
-        ;;
-    --workspace-sa)
-        shift
-        workspace_sa=$1
-        ;;
-    --airlock-key)
-        shift
-        airlock_key=$1
-        ;;
-    --workspace-key)
-        shift
-        workspace_key=$1
-        ;;
-    --operation-type)
-        shift
-        case $1 in
-        ingress)
-        ;;
-        egress)
-        ;;
-        *)
-            echo "operation-type must be 'ingress' or 'egress', not $1"
-            exit 1
-        esac
-        operation_type=$1
-        ;;
-    --file-path)
-        shift
-        file_path=$1
-        ;;
-    --pattern)
-        shift
-        pattern=$1
-        ;;
+    ingress)
+    ;;
+    egress)
+    ;;
     *)
-        echo "Unexpected argument: '$1'"
-        usage
-        ;;
+      echo "operation-type must be 'ingress' or 'egress', not $1"
+      exit 1
     esac
+    operation_type=$1
+    ;;
+  --file-path)
+    shift
+    file_path=$1
+    ;;
+  --pattern)
+    shift
+    pattern=$1
+    ;;
+  *)
+    echo "Unexpected argument: '$1'"
+    usage
+    ;;
+  esac
 
-    if [[ -z "$2" ]]; then
-      # if no more args then stop processing
-      break
-    fi
+  if [[ -z "$2" ]]; then
+    # if no more args then stop processing
+    break
+  fi
 
-    shift # remove the current value for `$1` and use the next
+  shift # remove the current value for `$1` and use the next
 done
 
 if [[ -z "$(az account show)" ]]; then
-    exit 1
+  exit 1
 fi
 
 if [[ -z "$airlock_sa" ]]; then
 	echo "Missing required argument --airlock-sa"
 	Help
-    exit 1
+  exit 1
 else
 	if [[ -z "$(az storage account show --name $airlock_sa)" ]]; then
 		exit 1
@@ -87,7 +86,7 @@ fi
 if [[ -z "$workspace_sa" ]]; then
 	echo "Missing required argument --workspace-sa"
 	Help
-    exit 1
+  exit 1
 else
 	if [[ -z "$(az storage account show --name $workspace_sa)" ]]; then
 		exit 1
@@ -97,12 +96,12 @@ fi
 if [[ -z "$operation_type" ]]; then
 	echo "Missing required argument --operation-type"
 	Help
-    exit 1
+  exit 1
 fi
 if [[ -z "$file_path" && -z "$pattern" ]]; then
-    echo 'You must provide either --file-path or --pattern'
+  echo 'You must provide either --file-path or --pattern'
 	Help
-    exit 1
+  exit 1
 fi
 
 rg=$(az storage account show --name ${airlock_sa} --output tsv --query "resourceGroup")
@@ -126,22 +125,22 @@ fi
 ip=$(curl ipecho.net/plain)
 
 if [[ -z "$rg" ]]; then
-    echo "Failed to return the resource group for storage account ${airlock_sa}.  Please check the name provided."
+  echo "Failed to return the resource group for storage account ${airlock_sa}.  Please check the name provided."
 	exit 1
 fi
 
 if [[ $operation_type == "egress" ]]; then
-    file_share="egress"
-    src_sa=$workspace_sa
-    src_key=$workspace_key
-    dest_sa=$airlock_sa
-    dest_key=$airlock_key
+  file_share="egress"
+  src_sa=$workspace_sa
+  src_key=$workspace_key
+  dest_sa=$airlock_sa
+  dest_key=$airlock_key
 else
-    file_share="ingress"
-    src_sa=$airlock_sa
-    src_key=$airlock_key
-    dest_sa=$workspace_sa
-    dest_key=$workspace_key
+  file_share="ingress"
+  src_sa=$airlock_sa
+  src_key=$airlock_key
+  dest_sa=$workspace_sa
+  dest_key=$workspace_key
 fi
 
 echo "Creating network rule on storage account $workspace_sa for $ip"
@@ -149,7 +148,7 @@ echo "Creating network rule on storage account $workspace_sa for $ip"
 result=$(az storage account network-rule add --account-name ${workspace_sa} --resource-group ${rg} --ip-address ${ip})
 
 if [ -z "$result" ]; then
-    echo "Failed to add $ip for storage account ${workspace_sa}.  Please check the name provided."
+  echo "Failed to add $ip for storage account ${workspace_sa}.  Please check the name provided."
 	exit 1
 fi
 
@@ -158,19 +157,19 @@ echo "Waiting for network rule to take effect"
 sleep 30s
 
 if [[ ! -z "$file_path" ]]; then
-    file=$(az storage file exists --path ${file_path} --share-name ${file_share} --account-name ${src_sa} --account-key ${src_key} --output tsv --query "exists")
+  file=$(az storage file exists --path ${file_path} --share-name ${file_share} --account-name ${src_sa} --account-key ${src_key} --output tsv --query "exists")
 
-    if [[ $file == "false" ]]; then
-        echo "Failed to find $file_path in share ${file_share} for storage account ${src_sa}.  Please check the details provided."
-    else
+  if [[ $file == "false" ]]; then
+    echo "Failed to find $file_path in share ${file_share} for storage account ${src_sa}.  Please check the details provided."
+  else
 		echo "Copying ${file_path} from storage account ${src_sa}, file share ${file_share}."
 		az storage file copy start --destination-path $file_path --destination-share $file_share --account-name $dest_sa --account-key $dest_key --source-account-name $src_sa --source-path $file_path --source-share $file_share --source-account-key $src_key
 	fi
 else
-    files=$(az storage file list --share-name ${file_share} --account-name ${src_sa} --account-key ${src_key} --output tsv --query "[].[name]")
+  files=$(az storage file list --share-name ${file_share} --account-name ${src_sa} --account-key ${src_key} --output tsv --query "[].[name]")
 
-    echo "Copying ${#files[*]} file(s) from storage account ${src_sa}, file share ${file_share}."
-    az storage file copy start-batch --pattern "$pattern" --source-account-name $src_sa --source-share $file_share --source-account-key $src_key --destination-share $file_share --account-name $dest_sa --account-key $dest_key
+  echo "Copying ${#files[*]} file(s) from storage account ${src_sa}, file share ${file_share}."
+  az storage file copy start-batch --pattern "$pattern" --source-account-name $src_sa --source-share $file_share --source-account-key $src_key --destination-share $file_share --account-name $dest_sa --account-key $dest_key
 fi
 
 echo "Removing network rule on storage account $workspace_sa for $ip"
