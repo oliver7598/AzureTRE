81d84cdffb7eae1ae515a1f30a8d546b8874f6ef
oliver7598 <oliver.a@live.co.uk>
2022-03-30 13:55:45 +0000

    Add ingress/egress mounts

--- templates/workspace_services/guacamole/user_resources/guacamole-azure-windowsvm/terraform/vm_config.ps1
+++ templates/workspace_services/guacamole/user_resources/guacamole-azure-windowsvm/terraform/vm_config.ps1
@@ -1,6 +1,8 @@
 if( ${SharedStorageAccess} -eq 1 )
 {
-  $Command = "net use z: \\${StorageAccountName}.file.core.windows.net\${FileShareName} /u:AZURE\${StorageAccountName} ${StorageAccountKey}"
+  $Command = @"net use z: \\${StorageAccountName}.file.core.windows.net\${FileShareName} /u:AZURE\${StorageAccountName} ${StorageAccountKey}
+  net use y: \\${StorageAccountName}.file.core.windows.net\ingress /u:AZURE\${StorageAccountName} ${StorageAccountKey}
+  net use x: \\${StorageAccountName}.file.core.windows.net\egress /u:AZURE\${StorageAccountName} ${StorageAccountKey}"
   $Command | Out-File  "C:\ProgramData\Start Menu\Programs\StartUp\attatch_storage.cmd" -encoding ascii
   Remove-Item -LiteralPath "C:\AzureData" -Force -Recurse
 }
